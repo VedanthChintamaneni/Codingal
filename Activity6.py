1615c22286f808ef257b6261550bd1a158837ca5
Vedanth.Chintamaneni <vedanth.chintamaneni@gmail.com>
2026-07-24 10:21:49 +0400

    commit

---
+++ Activity6.py
@@ -0,0 +1,25 @@
+name="Vedanth"
+age=14
+is_student=True
+weight=56.5
+
+print("Name:", name)
+print("Data type of name:", type(name))
+
+print("Age:", age)
+print("Data type of age:", type(age))
+
+print("Is Student:", is_student)
+print("Data type of is_student:", type(is_student))
+
+print("Weight:", weight)
+print("Data type of weight:", type(weight)) 
+
+print("\n After type casting:")
+age=str(age)
+print(age)
+print("Data type of age:", type(age))
+
+weight=int(weight)
+print(weight)
+print("Data type of weight:", type(weight))
