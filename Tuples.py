b279c5b5b60b2c7decc475a2814ddb06970e8249
Vedanth.Chintamaneni <vedanth.chintamaneni@gmail.com>
2026-09-12 08:53:40 +0400

    commit

---
+++ Tuples.py
@@ -0,0 +1,11 @@
+mixed_tuple = ("Codingal", 5 , False)
+print(mixed_tuple)
+
+int_tuple = (1 , 2 , 3 , 4)
+print(int_tuple)
+
+new_tuple = int_tuple + (9,)
+print(new_tuple)
+
+count_tuple = (10,11,12,13,14,14)
+print(count_tuple.count(14))
